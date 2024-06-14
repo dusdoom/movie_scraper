48443ff509022b138ddea1ca2dbfea1ae9aa39b9
marcos <dusdoomv2@gmail.com>
2024-06-14 18:44:12 -0300

    ref: updated rotten tomatoes spider

--- movie_scraper/movie_scraper/spiders/rotten_tomatoes.py
+++ movie_scraper/movie_scraper/spiders/rotten_tomatoes.py
@@ -1,14 +1,13 @@
 from scrapy import Spider
-from movie_scraper.items import Movie, Review, Critic
+from movie_scraper.items import Movie, Review, User
 import scrapy
 import json
 
 MOVIES_FILE = "/primary/60 projects/movie-scraper/movies_test.txt"
 
-AMOUNT_OF_REVIEWERS = 556
+AMOUNT_OF_USERS = 50
 
 # currently this heavily depends on the API instead of the website itself
-# that means that if the API changes, this spider will break i more work should be done
 # it would be better to create some functions that scrape the website directly
 class RottenTomatoesSpider(Spider):
     name = "rotten_tomatoes"
@@ -16,8 +15,6 @@ class RottenTomatoesSpider(Spider):
     custom_settings = {
         "DOWNLOAD_DELAY": "0.5",
         "RANDOMIZE_DOWNLOAD_DELAY": "True",
-        "CONCURRENT_REQUESTS": "16",
-        "RANDOM_UA_PER_PROXY": True,
     }
     
     def start_requests(self):
@@ -30,39 +27,59 @@ class RottenTomatoesSpider(Spider):
 
             yield scrapy.Request(movie_url, callback=self.parse)
 
+    # Gets the API endpoint to fetch the first batch of reviewers from a movie.
+    # This approach offloads the need to work with JavaScript and makes the process faster, altough it's very prune to errors.
     def parse(self, response):
-        # url to load reviews from critics
-        load_more_critics = response.selector.xpath("//load-more-manager/@endpoint").get()
-        yield scrapy.Request(f"https://www.rottentomatoes.com{load_more_critics}?pageCount={AMOUNT_OF_REVIEWERS}", callback=self._get_critic_pages)
+        # url to load more reviews from users
+        load_more_users = response.selector.xpath("//load-more-manager/@endpoint").get()
+        yield scrapy.Request(f"https://www.rottentomatoes.com{load_more_users}?pageCount={AMOUNT_OF_USERS}", callback=self._get_users)
 
-    def _get_critic_pages(self, response):
+
+    # Generates a request for each user in the response based on the API pagination.
+    def _get_users(self, response):
         response_json = json.loads(response.text)
-
         for review in response_json["reviews"]:
-            yield scrapy.Request(f"https://www.rottentomatoes.com/{review['criticPageUrl']}", callback=self._get_critic_reviews)
+            yield scrapy.Request(f"https://www.rottentomatoes.com/napi{review['criticPageUrl']}/movies", callback=self._parse_reviews)
 
-    def _get_critic_reviews(self, response):
-        critic = Critic()
-        # it's -2 because the website adds /movies at the end of the url
-        name = response.url.split("/")[-2]
-        url = response.url
+        if("hasNextPage" in response_json):
+            parsed_url = f"https://www.rottentomatoes.com{response_json["api"]}?after={response_json["endCursor"]}&pageCount={AMOUNT_OF_REVIEWERS}"
+            yield scrapy.Request(parsed_url, callback=self._get_users)
 
-        yield scrapy.Request(f"https://www.rottentomatoes.com/napi/critics/{name}/movies", callback=self._parse_reviews)
+    # Builds and yield a Movie item.
+    def _parse_movie(self, name, url):
+        movie = Movie()
+        movie["name"] = name
+        movie["url"] = url
+        yield movie
 
 
+    # Builds and yield a User item.
+    def _parse_user(self, name, url):
+        user = User()
+        user["name"] = name
+        user["url"] = url
+        user["source"] = "www.rottentomatoes.com"
+        # this should be properly handled as soon as audience is implemented
+        user["category"] = "critic" in url and "critic" or "audience"
+        yield user
+
+    # Builds and yield a Review item.
     def _parse_reviews(self, response):
         response_json = json.loads(response.text)
 
-        critic_name = response.url.split("/")[-2]
-        critic_url = f"https://www.rottentomatoes.com/{critic_name}"
+        user_name = response_json["vanity"]
+        user_url = f"https://www.rottentomatoes.com/{user_name}"
+
+        self._parse_user(user_name, user_url)
 
         for review in response_json["reviews"]:
+            self._parse_movie(review["mediaTitle"], review["mediaUrl"])
+            
             review_item = Review()
-            review_item["criticUrl"] = critic_url
+            review_item["userUrl"] = user_url
             review_item["positiveSentiment"] = review["tomatometerState"] == "fresh" 
             review_item["source"] = "www.rottentomatoes.com"
             review_item["date"] = review["date"]
-            review_item["criticName"] = critic_name
             review_item["fullReviewUrl"] = review["url"] if "url" in review else None
             review_item["movieUrl"] = review["mediaUrl"]
             review_item["rating"] = review["originalScore"] if "originalScore" in review else None
