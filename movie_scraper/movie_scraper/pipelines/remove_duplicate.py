a8119345fd970aedafca8d849f095767af3e9ea9
marcos <dusdoomv2@gmail.com>
2024-06-14 23:45:57 -0300

    feat: added pipelines

---
+++ movie_scraper/movie_scraper/pipelines/remove_duplicate.py
@@ -0,0 +1,15 @@
+from scrapy.exceptions import DropItem
+from scrapy import Item
+from itemadapter import is_item, ItemAdapter
+
+class RemoveDuplicate:
+    def __init__(self):
+        self.ids_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter["id"] in self.ids_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+            self.ids_seen.add(adapter["id"])
+            return item
